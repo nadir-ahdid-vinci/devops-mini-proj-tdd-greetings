c3f83ef6bbcd7477267b03a985a84524c0bb3b78
Gerard <gerard.licaj@student.vinci.be>
2023-11-12 16:30:59 +0100

    git init

---
+++ greeting.js
@@ -0,0 +1,5 @@
+function greet (name){
+    if(name === null) {return false}
+}
+
+export default {greet}
---
+++ greeting_test.js
@@ -0,0 +1,7 @@
+import { greet }   from "./greeting";
+import {expect, test} from '@jest/globals';
+
+
+test('greet', () => {
+    expect(greet("nadir")).toBe(true);
+});
