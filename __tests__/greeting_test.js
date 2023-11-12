2d5d034f481fed4c525eee3eb0182061de0de581
nadir-ahdid-vinci <nadir.ahdid@student.vinci.be>
2023-11-12 19:16:10 +0100

    reset project

--- __tests__/greeting_test.js
+++
@@ -1,7 +0,0 @@
-const greet = require('../greeting');
-
-// eslint-disable-next-line no-undef
-test('greet', () => {
-  // eslint-disable-next-line no-undef
-  expect(greet('nadir')).toBe('Hello, nadir')
-})
--- greeting.js
+++
@@ -1,7 +0,0 @@
-function greet (name) {
-  return 'Hello, ' + name
-}
-
-// "'tlo)-"'o)à-"'o)à-o"')ào")'àot'o"t"
-
-module.exports = greet;
